1c8ad806f70f7f6eccee106df99bddf180577ee0
xPapashi <dtomkowski1@gmail.com>
2023-08-31 17:54:54 +0100

    Add working form for books Books can now be added via a modal form Form will reset and close after each entry New books will appear at the bottom of the page

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //Creating an array for the books
-const myLibrary = [
-  "Story of an Apple", "George Bush", 420,
-  "Mercenary Ghost", "Chris Brown", 96
-];
+const myLibrary = [];
 
 //Searching for button class
 const btn = document.querySelector(".modal-btn");
@@ -18,39 +15,88 @@ function Book(title, author, pages, read) {
   };
 }
 
+//Form, Inputs and Buttons
+const form = document.querySelector('form');
+const submitBtn = document.querySelector(".submit-btn");
+const bTitle = document.getElementById("title");
+const bAuthor = document.getElementById("author");
+const bPages = document.getElementById("pages");
+const bRead = document.getElementById("read");
+
 //Adding books to the myLibrary array
 function addBookToLibrary() {
-    let title = prompt("Please enter the Title of the book");
-    let author = prompt("Please enter an Author of the book");
-    let pages = parseInt(prompt("Please enter the amount pages of pages"));
-    myLibrary.push(title, author, pages);
-    displayBook();
+  const dummyBook = new Book(
+    bTitle.value, 
+    bAuthor.value, 
+    bPages.value, 
+    bRead.checked);
+  myLibrary.push(dummyBook);
+  console.log(myLibrary);
+  displayBook();
+  // let title = prompt("Please enter the Title of the book");
+  // let author = prompt("Please enter an Author ofx the book");
+  // let pages = parseInt(prompt("Please enter the amount pages of pages"));
+  // myLibrary.push(title, author, pages);
+  // displayBook();
 }
 
 function displayBook() {
   const cards = document.querySelector(".cards");
   cards.innerHTML = "";
 
-  for (let i = 0; i < myLibrary.length; i += 3) {
-    const title = myLibrary[i];
-    const author = myLibrary[i + 1];
-    const pages = myLibrary[i + 2];
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
 
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `      
-      <h3>${title}</h3>
+      <h3>${book.title}</h3>
       <div class='details'>
-      <p>Author: ${author}</p>
-      <p>Pages: ${pages}</p>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p>Read: ${book.read ? "Yes" : "No"}</p>
       </div>`;
 
     cards.appendChild(card);
   }
 }
 
+/* MODAL */
+
+const modal = document.querySelector(".modal-popup");
+const overlay = document.querySelector(".overlay");
+
+//Opening modal
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+//Closing modal
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+//Listen for a click on "Add a book" button
+btn.addEventListener("click", openModal);
+
+//Listen for a click on the overlay
+overlay.addEventListener("click", closeModal);
+
+//Listen for a keydown
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+submitBtn.addEventListener("click", function (e) {
+  addBookToLibrary();
+  e.preventDefault();
+  closeModal();
+  form.reset();
+});
+
 //Initially display books
 displayBook();
-
-//Listen for button click and call "addBookToLibrary"
-btn.addEventListener("click", addBookToLibrary);
